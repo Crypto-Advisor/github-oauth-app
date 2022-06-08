8b4e27624826b3ccff5e046f7a817633b245ba5a
Adrien <adriennavarre@hotmail.com>
2022-06-08 15:18:29 -0700

    completed github oauth

--- starting_code/app.js
+++ starting_code/app.js
@@ -1,6 +1,6 @@
-/* 
- * Package Imports
-*/
+const session = require('express-session')
+const passport = require('passport')
+const GitHubStrategy = require('passport-github2').Strategy
 
 const path = require("path");
 require("dotenv").config();
@@ -22,7 +22,21 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 /*
  * Passport Configurations
 */
-
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done){
+    return done(null, profile)
+  })
+);
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+})
 
 
 
@@ -32,12 +46,18 @@ const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 /*
  *  Express Project Setup
 */
-
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(partials());
 app.use(express.json());
 app.use(express.static(__dirname + '/public'));
+app.use(session({
+  secret: 'codecademy',
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 
 
@@ -50,7 +70,11 @@ app.get('/', (req, res) => {
   res.render('index', { user: req.user });
 })
 
-app.get('/account', (req, res) => {
+app.get('/auth/github', passport.authenticate('github', {scope: ['user']}));
+
+app.get('/auth/github/callback', passport.authenticate('github', {failureRedirect: '/login', successRedirect: '/'}));
+
+app.get('/account', ensureAuthenticated, (req, res) => {
   res.render('account', { user: req.user });
 });
 
@@ -75,4 +99,11 @@ app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 /*
  * ensureAuthenticated Callback Function
 */
+function ensureAuthenticated (req, res, next){
+  if(req.isAuthenticated()){
+    return next()
+  }
+  res.redirect('/login')
+}
+
 
